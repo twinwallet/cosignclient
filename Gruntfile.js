9cd6086b250b9f0b514970905e2cf4261b667a00
Marco Polci <mpolci@gmail.com>
2015-11-02 22:55:27 +0100

    Replace make  with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask('default', ['browserify', 'uglify']);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        browserify: {
+            main: {
+                src: 'index-angular.js',
+                dest: 'cosignclient-angular.js'
+            }
+        },
+        uglify: {
+            main: {
+                files: {
+                    'cosignclient-angular.min.js': ['cosignclient-angular.js']
+                }
+            }
+        },
+        watch: {
+            files: 'index-angular.js',
+            tasks: ['default']
+        },
+        clean: ['cosignclient-angular.js', 'cosignclient-angular.min.js']
+    });
+};
