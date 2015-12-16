547cd06a2d2fcf6272a65b65ac59f209ccf55dfd
Marco Polci <mpolci@gmail.com>
2015-12-16 10:27:01 +0100

    Bump version to 0.6.3

--- cosignclient-angular.js
+++ cosignclient-angular.js
@@ -7,7 +7,7 @@ var CSClient = require('./lib/csclient');
 var NoticeBoard = require('./lib/noticeBoard');
 var NoticeExtBackup = require('./lib/noticeExtBackup');
 
-cscModule.constant('MODULE_VERSION', '0.6.2');
+cscModule.constant('MODULE_VERSION', '0.6.3');
 
 /**
  * Service factory.
@@ -184,7 +184,7 @@ CSClient.prototype.joinServerFromDevice1 = function (credentials, cb) {
     self._signRequest(credentials, params);
     self.httpRequest(params)
         .then(function successCB(response) {
-            if (response.data) {
+            if (!_.isEmpty(response.data)) {
                 var msg = JSON.stringify(response.data);
                 console.log('Unexpected http response: ' + msg);
                 return cb(new Error(msg));
@@ -899,27 +899,24 @@ module.exports = NoticeBoard;
 var lodash = require('lodash');
 var NoticeBoard = require('./noticeBoard');
 
-function ExtBackup(noticeBoard) {
-  if (!(this instanceof ExtBackup)) return new ExtBackup(noticeBoard);
-  this.noticeBoard = noticeBoard;
-
-  noticeBoard.on('newNotice/cancelBackup', ExtBackup.prototype._handlerCancelBackup.bind(this));
-  noticeBoard.on('newNotice/backupDevCompleted', ExtBackup.prototype._handlerBackupDevCompleted.bind(this));
-  noticeBoard.on('newNotice/backupDone', ExtBackup.prototype._handlerBackupDone.bind(this));
-}
-
-ExtBackup.prototype.startBackup = function (cb) {
-  var backupId = Math.random().toString(36).substring(8);
-  this.noticeBoard.postNotice('backupInProgress', {backupId: backupId}, cb);
+NoticeBoard.prototype.initExtBackup = function () {
+  this.on('newNotice/cancelBackup', NoticeBoard.prototype._handlerCancelBackup.bind(this));
+  this.on('newNotice/backupDevCompleted', NoticeBoard.prototype._handlerBackupDevCompleted.bind(this));
+  this.on('newNotice/backupDone', NoticeBoard.prototype._handlerBackupDone.bind(this));
 };
 
-ExtBackup.prototype._cleanBackup = function (backupId) {
+NoticeBoard.prototype.startBackup = function (cb) {
+  var backupId = Math.random().toString(36).substring(8);
+  this.postNotice('backupInProgress', {backupId: backupId}, cb);
+};
+
+NoticeBoard.prototype._cleanBackup = function (backupId) {
   var self = this;
   var ids;
   if (backupId) {
     ids = [backupId];
   } else {
-    ids = lodash(this.noticeBoard.notices)
+    ids = lodash(this.notices)
       .filter(function (n) {
         return n.data && n.data.backupId;
       })
@@ -929,60 +926,59 @@ ExtBackup.prototype._cleanBackup = function (backupId) {
       .uniq().value();
   }
   ids.forEach(function (id) {
-    self.noticeBoard.filterNotices({data: {backupId: id}}).forEach(function (n) {
+    self.filterNotices({data: {backupId: id}}).forEach(function (n) {
       if (n.type !== 'cancelBackup' && n.type !== 'backupDone')
-        self.noticeBoard.deleteNotice(n.id);
+        self.deleteNotice(n.id);
     });
   });
 };
 
-ExtBackup.prototype.cancelBackup = function () {
+NoticeBoard.prototype.cancelBackup = function () {
   var self = this;
-  self.noticeBoard.filterNotices({type: 'backupInProgress'}).forEach(function(n) {
-    self.noticeBoard.postNotice('cancelBackup', {backupId: n.data.backupId});
+  self.filterNotices({type: 'backupInProgress'}).forEach(function(n) {
+    self.postNotice('cancelBackup', {backupId: n.data.backupId});
   });
   self._cleanBackup();
 };
 
-ExtBackup.prototype._handlerCancelBackup = function (notice) {
-  if (notice.copayerId === this.noticeBoard.credentials.copayerId) return;
+NoticeBoard.prototype._handlerCancelBackup = function (notice) {
+  if (notice.copayerId === this.credentials.copayerId) return;
   if (!notice.data || !notice.data.backupId) {
     console.error('Invalid Notice: ' + JSON.stringify(backupNotice));
-    this.noticeBoard.deleteNotice(notice.id);
+    this.deleteNotice(notice.id);
   } else {
     this._cleanBackup(notice.data.backupId);
   }
 };
 
-ExtBackup.prototype.finishBackup = function (cb) {
+NoticeBoard.prototype.finishBackup = function (cb) {
   cb = cb || function() {};
-  var backupNotice = lodash.findWhere(this.noticeBoard.notices, {type: 'backupInProgress'});
+  var backupNotice = lodash.findWhere(this.notices, {type: 'backupInProgress'});
   if (!backupNotice) return cb(new Error('No backup notice'));
   var backupId = lodash.get(backupNotice, 'data.backupId');
   if (!backupId) {
     return console.error('Invalid Notice: ' + JSON.stringify(backupNotice));
   }
-  this.noticeBoard.postNotice('backupDevCompleted', {backupId: backupId}, cb);
+  this.postNotice('backupDevCompleted', {backupId: backupId}, cb);
 };
 
-ExtBackup.prototype._handlerBackupDevCompleted = function (notice) {
+NoticeBoard.prototype._handlerBackupDevCompleted = function (notice) {
   if (!notice.data || !notice.data.backupId)
     return console.error('Invalid Notice: ' + JSON.stringify(notice));
-  var completed = this.noticeBoard.filterNotices({
+  var completed = this.filterNotices({
     type: 'backupDevCompleted',
     data: {backupId: notice.data.backupId}
   });
   if (completed.length <= 1) return;
   if (completed[0].copayerId == completed[1].copayerId)
     return console.error('2 notices backupComplete from same device');
-  this.noticeBoard.postNotice('backupDone', null);
+  this.postNotice('backupDone', null);
 };
 
-ExtBackup.prototype._handlerBackupDone = function (notice) {
+NoticeBoard.prototype._handlerBackupDone = function (notice) {
   this._cleanBackup();
 };
 
-module.exports = ExtBackup;
 },{"./noticeBoard":3,"lodash":15}],5:[function(require,module,exports){
 (function (global){
 /*!
--- index-angular.js
+++ index-angular.js
@@ -6,7 +6,7 @@ var CSClient = require('./lib/csclient');
 var NoticeBoard = require('./lib/noticeBoard');
 var NoticeExtBackup = require('./lib/noticeExtBackup');
 
-cscModule.constant('MODULE_VERSION', '0.6.2');
+cscModule.constant('MODULE_VERSION', '0.6.3');
 
 /**
  * Service factory.
