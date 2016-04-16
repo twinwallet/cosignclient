70536a63df0ba95875f70aca73d90a272ec67ce5
Marco Polci <mpolci@gmail.com>
2016-04-16 12:36:02 +0200

    Bump version to 0.7.1

--- index-angular.js
+++ index-angular.js
@@ -6,7 +6,7 @@ var CSClient = require('./lib/csclient');
 var NoticeBoard = require('./lib/noticeBoard');
 var NoticeExtBackup = require('./lib/noticeExtBackup');
 
-cscModule.constant('MODULE_VERSION', '0.7.0');
+cscModule.constant('MODULE_VERSION', '0.7.1');
 
 /**
  * Service factory.
